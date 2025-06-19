738f5283071653f1cdca15ef145064831143d40c
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2025-06-19 08:38:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,10 @@ from os import environ
 API_ID = int(environ.get('API_ID', 0))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
+# Example format for Koyeb PostgreSQL URL:
+# postgres://username:password@host:port/database
+DATABASE_URL = environ.get("DATABASE_URL", "postgres://username:password@hostname:5432/dbnam")
+
 
 # Support multiple admin user IDs
 ADMINS = list(map(int, environ.get('ADMINS', '0').split()))

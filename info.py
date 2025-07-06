3d02d6ff9df9b2a86849a127140291c843902025
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2025-07-06 08:16:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,8 +4,10 @@ API_ID = int(environ.get('API_ID', 0))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 # Example format for Koyeb PostgreSQL URL:
-# postgres://username:password@host:port/database
-DATABASE_URL = environ.get("DATABASE_URL", "postgres://username:password@hostname:5432/dbnam")
+
+DATABASE_URI = os.environ("DATABASE_URI")
+DATABASE_NAME = os.environ("DATABASE_NAME", "autodelete")
+COLLECTION_NAME = os.environ("COLLECTION", "chats")
 
 
 # Support multiple admin user IDs
